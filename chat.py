a37ad5750a1f4e0b3944b19c0b829dd58a11f055
Adan <butto.adan2@gmail.com>
2026-02-25 11:39:55 +0200

    Added endpoints 'GET /', 'GET /api/chat/<room>' and a mock chat rooms directory

--- chat.py
+++ chat.py
@@ -1,3 +1,20 @@
-from flask import Flask
+import os
+
+from flask import Flask, abort, send_file
 
 app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+	return send_file("index.html")
+
+
+@app.get("/api/chat/<room>")
+def get_chat(room):
+	room_file = os.path.join("./chat_rooms_mock/", f"{room}")
+	if not os.path.isfile(room_file):
+		abort(404, description="Room not found")
+	with open(room_file, "r", encoding="utf-8") as handle:
+		return handle.read()
+
